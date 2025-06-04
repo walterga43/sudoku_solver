83358d3d0008430b1229b153f10b6e365e17ec33
walterga43 <waltergadams05@gmail.com>
2025-06-04 09:41:23 -0400

    Implemented load_puzzle and print_board

--- src/sudoku-solver.py
+++ src/sudoku-solver.py
@@ -1,14 +1,19 @@
-def __init__(self):
-  self.board = [[0 for _ in range(9)] for _ in range(9)]
+class SudokuSolver:
+  def __init__(self):
+    self.puzzle_grid = [[0 for _ in range(9)] for _ in range(9)]
 
-def get_board():
-  puzzle_file = open("sudoku_solver\data\puzzles.txt")
-  puzzle = puzzle_file.read()
-  print(puzzle)
-
-## def load_puzzle(self, puzzle_string):
-
-## def print_board(self):
+  def load_puzzle(self, puzzle_string):
+    for i in range(len(puzzle_string)):
+      row = i // 9
+      col = i % 9
+      if puzzle_string[i] != ".":
+        self.puzzle_grid[row][col] = int(puzzle_string[i])
+  
+  def print_board(self):
+    for r in range(9):
+      for c in range(9):
+        print(self.puzzle_grid[r][c], end=" ")
+      print()
 
 ## def is_valid_move(self, row, col, num):
 
@@ -17,4 +22,7 @@ def get_board():
 ## def solve(self):
 
 if __name__ == "__main__":
-  get_board()
+  puzzle = "53..7....6..195....98....6.8...6...34..8.3..17...2...6.6....28....419..5....8..79"
+  solver = SudokuSolver()
+  solver.load_puzzle(puzzle)
+  solver.print_board()
